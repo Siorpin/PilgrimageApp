05b7e28b2cfddf2b471bfde2795cc1bb13048540
siorpin <kubasierpien05@gmail.com>
2025-01-21 23:57:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pielgrzymkabielskozywiecka/core/navigation/NavHost.kt
@@ -0,0 +1,30 @@
+package com.example.pielgrzymkabielskozywiecka.core.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.pielgrzymkabielskozywiecka.pielgrzymka.presentation.homeScreen.HomeScreen
+
+@Composable
+fun AppNavigation(
+    navController: NavHostController,
+    startDestination: Screen,
+    padding: PaddingValues,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+            .padding(padding)
+    ) {
+        composable(route = startDestination.name){
+            HomeScreen()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/pielgrzymkabielskozywiecka/core/navigation/Screen.kt
@@ -0,0 +1,5 @@
+package com.example.pielgrzymkabielskozywiecka.core.navigation
+
+enum class Screen {
+    HOME
+}
