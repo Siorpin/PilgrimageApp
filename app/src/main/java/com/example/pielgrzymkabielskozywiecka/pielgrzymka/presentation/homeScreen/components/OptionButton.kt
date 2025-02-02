a1207ac8eabfa3d5e6e5ed7ae4c587181c2d5fc6
siorpin <kubasierpien05@gmail.com>
2025-02-02 16:08:14 +0100

    added navhost to homescreen

--- app/src/main/java/com/example/pielgrzymkabielskozywiecka/pielgrzymka/presentation/homeScreen/components/OptionButton.kt
+++ app/src/main/java/com/example/pielgrzymkabielskozywiecka/pielgrzymka/presentation/homeScreen/components/OptionButton.kt
@@ -1,15 +1,10 @@
 package com.example.pielgrzymkabielskozywiecka.pielgrzymka.presentation.homeScreen.components
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
--- app/src/main/java/com/example/pielgrzymkabielskozywiecka/pielgrzymka/presentation/homeScreen/components/OptionsRow.kt
+++ app/src/main/java/com/example/pielgrzymkabielskozywiecka/pielgrzymka/presentation/homeScreen/components/OptionsRow.kt
@@ -3,26 +3,19 @@ package com.example.pielgrzymkabielskozywiecka.pielgrzymka.presentation.homeScre
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.pielgrzymkabielskozywiecka.ui.theme.Poppins
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 
 @Composable
 fun OptionsRow(
+    navHost: NavHostController,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -40,14 +33,14 @@ fun OptionsRow(
             OptionButton(
                 buttonText = "Galeria",
                 color = Color(0xFFA6AAF3),
-                onClick = {},
+                onClick = { navHost.navigate("") }, //TODO
                 modifier = Modifier
                     .weight(1f)
             )
             OptionButton(
                 buttonText = "Strona internetowa",
                 color = Color(0xFFC4C5FF),
-                onClick = {},
+                onClick = { navHost.navigate("") }, //TODO
                 modifier = Modifier
                     .weight(1f)
             )
