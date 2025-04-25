7a525d84fc0260c3843ab412188abc3a2da2bea2
siorpin <kubasierpien05@gmail.com>
2025-04-25 20:56:41 +0200

    created PopUp

--- app/src/main/java/com/example/pielgrzymkabielskozywiecka/pielgrzymka/presentation/shared/PopUp.kt
+++ app/src/main/java/com/example/pielgrzymkabielskozywiecka/pielgrzymka/presentation/shared/PopUp.kt
@@ -1,9 +1,15 @@
 package com.example.pielgrzymkabielskozywiecka.pielgrzymka.presentation.shared
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.shrinkOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,6 +17,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
@@ -25,17 +32,30 @@ fun PopUp(
 ) {
     Box(
         modifier = Modifier
-            .fillMaxSize(if (activated) 1f else 0f)
-            .background(MaterialTheme.colorScheme.background.copy(alpha = 0.9f))
-            .pointerInput(Unit) { onBackgroundTap() }
+            .fillMaxSize()
     ) {
         AnimatedVisibility(
             visible = activated,
-            enter = scaleIn(),
-            exit = shrinkOut(),
-            modifier = modifier
-                .fillMaxSize()
-                .pointerInput(Unit){}
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background.copy(alpha = 0.7f))
+                    .clickable(
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() }
+                    ) {
+                        onBackgroundTap()
+                    }
+            )
+        }
+        AnimatedVisibility(
+            visible = activated,
+            enter = slideInHorizontally { -it },
+            exit = slideOutHorizontally { -it },
+            modifier = modifier.align(Alignment.Center)
         ) {
             Card(
                 colors = CardDefaults.cardColors(
@@ -47,6 +67,7 @@ fun PopUp(
                         horizontal = 40.dp
                     )
                     .fillMaxSize()
+                    .clickable(enabled = false) {}
             ) {
                 Box(
                     contentAlignment = Alignment.Center,
