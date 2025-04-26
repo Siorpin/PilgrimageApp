7d514a674ff6cfa5eeebba15ecc66676d7d52300
siorpin <kubasierpien05@gmail.com>
2025-04-26 11:24:10 +0200

    bug fix

--- app/src/main/java/com/example/pielgrzymkabielskozywiecka/pielgrzymka/presentation/shared/OptionButton.kt
+++ app/src/main/java/com/example/pielgrzymkabielskozywiecka/pielgrzymka/presentation/shared/OptionButton.kt
@@ -6,6 +6,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -15,6 +19,11 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pielgrzymkabielskozywiecka.ui.theme.Poppins
+import kotlinx.coroutines.coroutineScope
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun OptionButton(
@@ -23,8 +32,20 @@ fun OptionButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val coroutineScope = rememberCoroutineScope()
+    var isButtonEnabled by remember {
+        mutableStateOf(true) }
+
     Button(
-        onClick = { onClick() },
+        onClick = {
+            if(isButtonEnabled) {
+                coroutineScope.launch{
+                    isButtonEnabled = false
+                    onClick()
+                    delay(400)
+                    isButtonEnabled = true
+                }
+            } },
         shape = RoundedCornerShape(16.dp),
         elevation = ButtonDefaults.buttonElevation(4.dp),
         colors = ButtonDefaults.buttonColors(
