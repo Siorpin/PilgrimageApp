4340b89bd61482ad33224d5c2e85079226c71937
siorpin <kubasierpien05@gmail.com>
2025-05-10 00:34:24 +0200

    fast bug fix //added scroll

--- app/src/main/java/com/example/pielgrzymkabielskozywiecka/pielgrzymka/presentation/prayerScreen/PrayerScreen.kt
+++ app/src/main/java/com/example/pielgrzymkabielskozywiecka/pielgrzymka/presentation/prayerScreen/PrayerScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +27,7 @@ fun PrayerScreen(
         modifier = modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
+            .verticalScroll(rememberScrollState())
     ) {
         if (title != null && lyrics != null) {
             SecondaryScreenTopBar(
